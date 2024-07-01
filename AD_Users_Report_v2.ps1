b0aced57d5f815ee8a96c21b592348d7ccf7ff89
AU-Mark <167436500+AU-Mark@users.noreply.github.com>
2024-07-01 17:05:14 -0400

    Update AD_Users_Report_v2.ps1

--- AD_Users_Report_v2.ps1
+++ AD_Users_Report_v2.ps1
@@ -889,7 +889,6 @@ try {
 	Write-Color -Text "|__________________________________________________________________________________________|" -Color White -BackgroundColor Black -HorizontalCenter $True
 	Write-Color -Text "Script: ","User Audit Report" -Color Yellow,White -HorizontalCenter $True -LinesBefore 1
 	Write-Color -Text "Author: " ,"Mark Newton" -Color Yellow,White -HorizontalCenter $True -LinesAfter 1
-	Write-Color -Text "Checking for optional but recommended PowerShell modules" -ShowTime
 
 	if ($PSVersionTable.PSVersion.Major -lt 5) {
 		$ImportExcel = $False
@@ -915,6 +914,8 @@ try {
 	}
 
 	if ($SupportedPS) {
+		Write-Color -Text "Checking for optional but recommended PowerShell modules" -ShowTime
+
 		# Check for and prompt to install ImportExcel module
 		$ImportExcel,$RemoveImportExcel,$IEUntrustPSGallery,$IERemovePSGallery,$IERemoveNuGet = Initialize-ImportExcel
 
